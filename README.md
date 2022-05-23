1e21b3f8368dc8fe7afe76832514c1f39cd47115
Liz  GingerKiwi.Dev <71160864+GingerKiwi@users.noreply.github.com>
2022-05-22 21:08:50 -0400

    updated files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 2022-05-22
- App for front end dev application. Will be further developed in another repo to support grades 7 & 8 in the new 2022 Ontario Science Curriculum.
+
+ An app for front end dev application.
+
+ It will be further developed in another repo to support grades 7 & 8 in the new 2022 Ontario Science Curriculum.
+
+ As a former special educator starting a new career in information technology, I'd like to support teachers and students by developing a quick and easy to access lesson plan and activities on artifical intelligence. The future app will have a choice of themes including one that is low vision and dyslexia friendly. There will also be functionality to monitor and/or restrict students from asking inappropriate questions (because they will try).
---
+++ js/script.js
@@ -0,0 +1,74 @@
+// import OPEN_API_KEY from "./apikey.js";
+//Note: the repo name is purposely vague as I haven't managed to run the app with the key hidden
+// I did try on Netlify:    cd js && echo -e "const OPEN_API_KEY = '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';\n\nexport default OPEN_API_KEY;" > test.js"
+const OPEN_API_KEY = "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";
+let query = document.getElementById("question-input");
+const answercontainer = document.getElementById("answercontainer");
+const enginechoice = document.getElementById("engine-choice");
+let engine = 'text-ada-001';
+
+
+// event listeners for buttons
+document.getElementById("submit-button").addEventListener("click", function(e){
+    e.preventDefault();
+    fetchQuestion();
+});
+
+// event listener function to change engine choice
+engineSelector.addEventListener("change", function(e){
+    e.preventDefault();
+    responseContainer.innerHTML = "";
+    query.value = "";
+    if(document.getElementById('engine').value == 'Ada'){
+        engine = 'text-ada-001';
+    };
+    if(document.getElementById('engine').value == 'Curie'){
+        engine = 'text-curie-001';
+    };
+    if(document.getElementById('engine').value == 'Babbage'){
+        engine = 'text-babbage-001';
+    };
+    if(document.getElementById('engine').value == 'Davinci'){
+        engine = 'text-davinci-002';
+    };
+
+});
+
+// function to fetch data responses from Open AI API
+function fetchQuestion(){
+    const data = {
+        prompt: query.value,
+        temperature: 0.5,
+        max_tokens: 75,
+        top_p: 1.0,
+        frequency_penalty: 0.5,
+        presence_penalty: 0.0,
+    };
+    fetch(`https://api.openai.com/v1/engines/${engine}/completions`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${OPEN_API_KEY}`
+        },
+        body: JSON.stringify(data),
+    })
+    .then(res => res.json())
+    .then(data => {
+        return displayQandA(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+};
+
+// function to display responses fetched from Open AI API
+function displayQandA(data){
+    answercontainer.innerHTML += `
+    <div id="answerCard" class="answer-card">
+      <p id = "questiontitle"> <span id="questionName" class="uppercase"> Your Question: </span> ${query.value}</p>
+      <p id = "answerresult"> <span id="answerName" class="uppercase"> The AI's Answer: </span> ${data.choices[0].text}</hp>
+    </div>
+   `
+};
+
+
